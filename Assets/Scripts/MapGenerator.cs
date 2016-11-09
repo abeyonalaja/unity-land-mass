e2358482c7f74fabf0b5289c92538a44ca8bc565
Abey Onalaja <abey.onalaja@gmail.com>
2016-11-09 21:41:18 +0000

    Add MapGenerator and MapDisplay scripts

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public int mapWidth, mapHeight;
+	public float noiseScale;
+
+	public void GenerateMap() {
+		float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+
+		MapDisplay display = FindObjectOfType<MapDisplay>();
+		display.DrawNoiseMap(noiseMap);
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
